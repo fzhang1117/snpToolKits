908bb27968fc97fbc8edea6a7a031969892ebdcc
fzhang <fzhang@node102.(none)>
2018-05-10 22:01:03 +0800

    zf

---
+++ script/xQTL_identify/script/CandidateGene_Search.py
@@ -0,0 +1,49 @@
+## CandidateGene_Search.py ##
+## zhangfei <zhangfei-123@foxmail.com> ##
+## 2018-05-10 ##
+
+import sys, re
+from itertools import islice
+
+## fl_xQTL_final: the xQTL_final info after single snp and LD filter
+## fl_anno: GFF3 annotation file
+
+## output:
+## trait, condition, xQTL, chr, start, end, lead_snp, leadp, gene
+
+fl_xQTL_final = sys.argv[1]
+fl_anno = sys.argv[2]
+flanking_length = int(sys.argv[3])
+fl_out = sys.argv[4]
+
+## build gene dictionary
+## {chrom:{gene:[gene, chrom, gene_start, gene_end]}}
+with open(fl_anno, 'r') as fh_anno:
+	dic_anno = {}
+	for line in fh_anno:
+		line = line.strip('\n').split('\t')
+		chrom, gene_start, gene_end = line[0][3:], int(line[3]), int(line[4])
+		gene = re.split(';|=', line[8])[1]
+		if dic_anno.get(chrom) == None:
+			dic_anno[chrom] = {}
+			dic_anno[chrom][gene] = [gene, chrom, gene_start, gene_end]
+		else:
+			dic_anno[chrom][gene] = [gene, chrom, gene_start, gene_end]
+
+with open(fl_out, 'a') as fh_out:
+	with open(fl_xQTL_final, 'r') as fh_xQTL_final:
+		for line in islice(fh_xQTL_final, 1, None):
+			line = line.strip('\n').split('\t')
+			trait, condition, xQTL, xQTL_chr, lead_snp, xQTL_start, xQTL_end, leadp = line[0], line[1], line[2], line[3], line[8], int(line[4]), int(line[5]), line[9]
+			location = int(re.split('_', lead_snp)[1])
+			if dic_anno.get(xQTL_chr) is not None:
+				dic_xQTL_chr = dic_anno[xQTL_chr]
+				bound_left = location - flanking_length
+				bound_right = location + flanking_length
+				for gene in dic_xQTL_chr.keys():
+					gene_start, gene_end = dic_xQTL_chr[gene][2], dic_xQTL_chr[gene][3]
+					if gene_start >= bound_left and gene_end <= bound_right:
+						output = [trait, condition, xQTL, xQTL_chr, xQTL_start, xQTL_end, lead_snp, leadp, gene]
+						output = [str(i) for i in output]
+						fh_out.writelines('\t'.join(output))
+						fh_out.writelines('\n')
--- script/xQTL_identify/script/run.sh
+++ script/xQTL_identify/script/run.sh
@@ -24,8 +24,11 @@ singlefilter=$7
 ## the threshold of LD
 threshold=$8
 
+## the flanking_region
+flanking=$9
+
 ## for example
-## sh run.sh ../data/mlm_GC_drought_5.00.txt ../data/mlm_GC_normal_5.00.txt ../data/sorted_mGWAS_sig_snp.all.hmp ../../../data/ZmB73_5b_FGS_GeneOnly.gff3 10000 ../result/mlm_GC_5.00_20kb_0.1 T 0.1
+## sh run.sh ../data/mlm_GC_drought_5.00.txt ../data/mlm_GC_normal_5.00.txt ../data/sorted_mGWAS_sig_snp.all.hmp ../../../data/ZmB73_5b_FGS_GeneOnly.gff3 10000 ../result/mlm_GC_5.00_20kb_0.1_100kb T 0.1 100000
 
 python ./xQTL_group.py $fl_drought $fl_normal $wing $prefix $fl_gff
 sort -k1,1 -k2,2 -k3,3n ${prefix}_xQTL_summary.txt > ${prefix}_xQTL_summary_sorted.txt
@@ -38,3 +41,4 @@ cat ${prefix}_drought_xQTL_ld.txt ${prefix}_normal_xQTL_ld.txt > ${prefix}_xQTL_
 rm -rf ${prefix}_xQTL_summary_sorted.txt
 Rscript ./xQTL_merge.R ${prefix}_xQTL_summary.txt ${prefix}_xQTL_ld.txt ${prefix} ${threshold} $singlefilter
 rm -rf ${prefix}_normal_xQTL_ld.txt ${prefix}_drought_xQTL_ld.txt ${prefix}_xQTL_drought.txt ${prefix}_xQTL_normal.txt
+python CandidateGene_Search.py ${prefix}_final_summary.txt $fl_gff $flanking ${prefix}_candidate_temp.txt
