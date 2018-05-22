f46ae14773a8d8fc0ea5d7a730fb23022ae1b0fa
zf <zhangfei-123@foxmail.com>
2018-05-22 22:52:36 +0800

    zf

--- script/xQTL_identify/script/xQTL_CandidatepGWAS_summary.py
+++ script/xQTL_identify/script/xQTL_CandidatepGWAS_summary.py
@@ -8,6 +8,7 @@ from itertools import islice
 
 fl_CandidateGene = sys.argv[1]
 fl_pGWASSummary = sys.argv[2]
+fl_output = sys.argv[3]
 
 with open(fl_CandidateGene, 'r') as fh_CandidateGene:
     dic_CandidateGene = {}
@@ -15,7 +16,7 @@ with open(fl_CandidateGene, 'r') as fh_CandidateGene:
         line = line.strip('\n').split('\t')
         gene = line[8]
         ## output = [cpd, condition, xQTL, xQTL_chr, xQTL_start, xQTL_end, leadsnp, leadp, CandidateGene, GeneLocation, Distance, Pathway, PathwayAnnotation, v4_gene_model]
-        output = line[0:10] + line[12: 14] + [line[15]]
+        output = line[0:11] + line[12: 14] + [line[15]]
         if dic_CandidateGene.get(gene) is None:
             dic_CandidateGene[gene] = [output]
         else:
@@ -37,8 +38,8 @@ dic_summary, trait_list = {}, []
 for line in result_merge:
     gene, cpd, condition = line[0], line[8], line[9]
     key = gene + '-' + cpd + '-' + condition
-    xQTL, leadp_cpd, location, pathway, pathwayAnno, ZmV4, genesymbol, altName, zma_alias, zma_description, ara_ortholog, ara_alias, ara_annotation = line[10], line[15], line[17], line[18], line[19], line[20], line[21], line[26], line[27], line[28], line[29], line[30], line[31]
-    key_anno = [xQTL, leadp_cpd, location, pathway, pathwayAnno, ZmV4, genesymbol, altName, zma_alias, zma_description, ara_ortholog, ara_alias, ara_annotation]
+    xQTL, leadp_cpd, location, distance, pathway, pathwayAnno, ZmV4, genesymbol, altName, zma_alias, zma_description, ara_ortholog, ara_alias, ara_annotation = line[10], line[15], line[17], line[18], line[19], line[20], line[21], line[22], line[26], line[27], line[28], line[29], line[30], line[31]
+    key_anno = [xQTL, leadp_cpd, location,distance, pathway, pathwayAnno, ZmV4, genesymbol, altName, zma_alias, zma_description, ara_ortholog, ara_alias, ara_annotation]
     trait, snp_pGWAS, leadp_pGWAS = line[1], line[2], float(line[5])
     trait_list.append(trait)
     if dic_summary.get(key) is None:
@@ -55,5 +56,26 @@ for line in result_merge:
             dic_summary[key]['summary_leadp'][trait] = min(leadp_pGWAS, leadp_pGWAS)
 
 trait_list = sorted(list(set(trait_list)))
-print dic_summary
-print trait_list
+
+title = ['gene', 'cpd', 'condition', 'xQTL', 'p_cpd', 'gene:leadp_location', 'gene:leadp_distance', 'pathwayID', 'pathwayAnno', 'gene_v4', 'genesymbol', 'altName', 'zma_alias', 'zma_description', 'ara_ortholog', 'ara_alias', 'ara_annotation'] + ['snpnum:' + i for i in trait_list] + ['minp:' + i for i in trait_list ]
+output = [title]
+for key in dic_summary.keys():
+    key_split = key.split('-')
+    key_anno = dic_summary[key]['key_anno']
+    dic_snp, dic_p = {}, {}
+    for trait in trait_list:
+        if dic_summary[key]['summary_snp'].get(trait) is not None:
+            dic_snp[trait] = dic_summary[key]['summary_snp'][trait]
+            dic_p[trait] = dic_summary[key]['summary_leadp'][trait]
+        else:
+            dic_snp[trait] = 0
+            dic_p[trait] = '-'
+    entry_snp = [dic_snp[i] for i in sorted(dic_snp.keys())]
+    entry_p = [dic_p[i] for i in sorted(dic_p.keys())]
+    entry = key_split + key_anno + entry_snp + entry_p
+    entry = [str(i) for i in entry]
+    output.append(entry)
+
+#with open()
+output = ['\t'.join(i) for i in output]
+print '\n'.join(output)
